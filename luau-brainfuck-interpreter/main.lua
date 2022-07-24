14befb2472814d5fe108392b86c37457c93e1334
tally-hall <109919325+tally-hall@users.noreply.github.com>
2022-07-24 14:56:06 -0400

    Update main.lua

--- luau-brainfuck-interpreter/main.lua
+++ luau-brainfuck-interpreter/main.lua
@@ -1,4 +1,4 @@
--- this is a brainfuck interpreter in luau, roblox's version of lua. ( https://github.com/Roblox/luau )
+-- this is a brainfuck interpreter in luau, a dialect of lua. ( https://github.com/Roblox/luau )
 
 local bf = { }
 
